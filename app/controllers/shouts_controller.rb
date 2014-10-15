0a8b8db18582b1475a5d74abddc3ba25fb3c2e65
Nathan Beck <nathan.beck10@gmail.com>
2014-10-15 10:28:42 -0400

    Users can create Shouts with TextSubjects

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -2,24 +2,7 @@ class ShoutsController < ApplicationController
   before_action :require_login
   
   def index
-    @shout = Shout.new
+    @text_subject = TextSubject.new
     @timeline = current_user.timeline
-    @following_relationship = FollowingRelationship.new
-
-    @users_i_follow = current_user.followed_users
-    @users_i_do_not_follow = User.where.not(id: current_user.followed_user_ids)
-  end
-
-  def create
-    #Shout.create(user_id: current_user.id, body: params[:shout][:body])
-    current_user.shouts.create(shout_params)
-
-   redirect_to shouts_path
-  end
-
-  private
-  
-  def shout_params
-    params.require(:shout).permit(:body)
   end
 end
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -2,8 +2,11 @@ class Shout < ActiveRecord::Base
   belongs_to :user
   belongs_to :subject, polymorphic: true
 
-  validates :body, presence: true
   validates :user_id, presence: true
 
   belongs_to :user
+
+  def body=(new_body)
+    self[:body]=(new_body.upcase)
+  end
 end
